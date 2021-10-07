cf1e8167d81bfae8ec24c807222fbeff28749a70
Denis <veremyov.of@gmail.com>
2021-10-07 12:33:42 +0300

    review code

--- src/components/Loadmore.jsx
+++ src/components/Loadmore.jsx
@@ -2,28 +2,11 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import "../css/Loadmore.css";
 import { loadmoreAction } from '../store/asyncActions/loadmore';
-// import { renderBooksAction } from '../store/booksReducer';
-// import { clearResultAction } from './../store/booksReducer';
 
 export default function Loadmore() {
     const dispatch = useDispatch();
     const books = useSelector(state => state.books);
-
-    // async function showBooks() {
-    //     loadingBooks()
-    //     let result = [];
-    //     await fetch(`https://www.googleapis.com/books/v1/volumes?q=${books.search}&key=${books.apiKey}&maxResults=${books.maxResults}&orderBy=${books.sorting}&startIndex=${books.books.length}`)
-    //         .then(response => response.json())
-    //         .then(json => result = json.items)
-    //         .then(() => dispatch(loadmoreAction(result)))
-    //         .then(() => dispatch(renderBooksAction()))
-    //         .then(() => dispatch(clearResultAction()))
-    // }
-
-    // const loadingBooks = () => {
-    //     dispatch({type: 'LOADING'})
-    // }    
-    
+ 
     return (
         <div  className="loadmore-wrapp">
             <button className="loadmore-btn" onClick={() => dispatch(loadmoreAction(books))}>Load more</button>
